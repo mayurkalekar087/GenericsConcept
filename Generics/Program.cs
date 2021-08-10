2bef9567fb42b04ebc8fa785f4c0bc77a99eab95
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-10 22:22:41 +0530

    feat:Generics Concept

---
+++ Generics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1,2,3,4,5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'o' };
+            Console.WriteLine("--------------------");
+            new printArray<int>(intArray).toPrint();
+            new printArray<double>(doubleArray).toPrint();
+            new printArray<char>(charArray).toPrint();
+           
+        }
+       
+    }
+}
+
---
+++ Generics/printArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class printArray<T>
+    {
+        private T[] inputArray;//instance variable
+        public printArray(T[] inputArray) // constructor
+        {
+            this.inputArray = inputArray;
+        }
+        public void toPrint() //method
+        {
+            foreach (var element in inputArray)
+            {
+
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------");
+        }
+    }
+}
